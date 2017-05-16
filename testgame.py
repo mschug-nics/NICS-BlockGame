53490454f75c5e82f77c522856c566be3b15f08a
Matthew Schug <mschug@nicsroyals.com>
2017-05-16 10:59:07 -0700

    Slowed down button press to reasonable speed.

--- testgame.py
+++ testgame.py
@@ -4,14 +4,30 @@ pygame.display.set_caption("Example Program!")
 screen = pygame.display.set_mode((640, 480))
 myrect = pygame.Rect(0,0,640,480)
 image = pygame.image.load("player.png")
-screen.fill((0, 0, 0))
-screen.blit(image,myrect)
-pygame.display.flip()
 running=True
+clock = pygame.time.Clock()
 while running:
+        clock.tick(60)
+        screen.fill((0, 0, 0))
+        screen.blit(image,myrect)
+        pygame.display.flip()        
         events = pygame.event.get()
         for event in events:
-                if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                                pygame.quit()
-                                running=False
+                if event.type == pygame.QUIT:
+                        running=False
+        key = pygame.key.get_pressed()
+        if key[pygame.K_ESCAPE]:
+                pygame.quit()
+                running=False
+        if key[pygame.K_UP]:
+                myrect.y=myrect.y-5
+        if key[pygame.K_DOWN]:
+                myrect.y=myrect.y+5
+        if key[pygame.K_LEFT]:
+                myrect.x=myrect.x-5
+        if key[pygame.K_RIGHT]:
+                myrect.x=myrect.x+5
+        if key[pygame.K_SPACE]:
+                pygame.quit()
+                 running=False
+                                
